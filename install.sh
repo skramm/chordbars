36a1c275f579eeb90556bad3aa2f07d7751f47fa
skramm <sebastien.kramm@univ-rouen.fr>
2018-11-11 22:44:12 +0100

    workable, added tempo related commands

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# install script. Please adapt to your local config
+
+mkdir -p /usr/local/texlive/texmf-local/tex/latex/chordbars/
+cp chordbars.sty /usr/local/texlive/texmf-local/tex/latex/chordbars/
+
+
