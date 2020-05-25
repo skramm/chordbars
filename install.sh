dd362fe50f76d20c2c495d095f44961d8ed33516
skramm <sebastien.kramm@univ-rouen.fr>
2020-05-25 10:40:09 +0200

    added nex command

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# install script. Please adapt to your local config
+# install script. Please adapt to your local config, this here should be ok for texlive (2020 at least)
 
-mkdir -p /usr/local/texlive/texmf-local/tex/latex/chordbars/
-cp chordbars.sty /usr/local/texlive/texmf-local/tex/latex/chordbars/
+#mkdir -p /usr/local/texlive/texmf-local/tex/latex/chordbars/
+mkdir -p /usr/local/share/texmf/tex/latex/chordbars/
 
+#cp chordbars.sty /usr/local/texlive/texmf-local/tex/latex/chordbars/
+cp chordbars.sty /usr/local/share/texmf/tex/latex/chordbars/
+
+texhash
 
