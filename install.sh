27612034901dc8ef6ca64cd43139650867a33c4c
skramm <sebastien.kramm@univ-rouen.fr>
2021-03-28 18:46:00 +0200

    fixed font issue, + misc.

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # install script. Please adapt to your local config, this here should be ok for texlive (2020 at least)
 
-#mkdir -p /usr/local/texlive/texmf-local/tex/latex/chordbars/
-mkdir -p /usr/local/share/texmf/tex/latex/chordbars/
+#loc=/usr/local/texlive/2020/texmf-dist/tex/latex/chordbars/
+loc=/usr/local/texlive/texmf-local/tex/latex/chordbars/
 
-#cp chordbars.sty /usr/local/texlive/texmf-local/tex/latex/chordbars/
-cp chordbars.sty /usr/local/share/texmf/tex/latex/chordbars/
+
+mkdir -p $loc
+cp chordbars.sty $loc
 
 texhash
 
